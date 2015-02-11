f17f3a2d55812e3954664b8c70613e4f20d2e880
Marty <martin.l.billard@gmail.com>
2015-02-11 12:36:20 +0100

    Ajout des models de core et social

--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    # first_name, last_name and username, password, email are already defined
+    avatar = models.ImageField(upload_to="avatars/", default="avatars/default.png")
+    banner = models.ImageField(upload_to="banners/", blank=True)
+    bio = models.TextField(max_length=300)
+    birthdate = models.DateTimeField()
+    place = models.ForeignKey("Place")
+    privacy = models.BooleanField(default="true") # TODO : Voir avec Enguerrand pour des conseils d'utilisation
+
+class Place(models.Model):
+    city = models.CharField(max_length=163)
+    country = models.CharField(max_length=200)
+
+class UserLink(models.Model):
+    user = models.ForeignKey("User", related_name="links")
+    type = models.ForeignKey("UserLinkTypes")
+    url = models.URLField()
+
+
+class UserLinkTypes(models.Model):
+    name = models.CharField(max_length = 255)
+    icon = models.ImageField(upload_to="socialIcons/", blank=True)
+
+
