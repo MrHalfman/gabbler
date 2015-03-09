cdf7813910605892f4c70d5fdc51db41d320bf44
Marty <martin.l.billard@gmail.com>
2015-03-10 00:01:21 +0100

    Finition de la version alpha du home (fixes #1)

--- core/views.py
+++ core/views.py
@@ -7,27 +7,31 @@ from django.contrib import messages
 
 
 def home(request):
-    return render(request, "index.html")
-
-def connect(request) :
     if request.method == "POST":
         """
             POST.get() diffère de POST[] dans le sens où si le champ
             est vide, il remplacera le contenu par "None" au lieu de retourner
             une erreur.
         """
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(username = username, password = password)
+        if request.POST.get("redirection") == "connect" :
+            return HttpResponseRedirect("/connect") # Redirection en cas d'autentification
 
-        if user:
-            login(request, user) # Fait la variable de session avec l'utilisateur dedans
-            return HttpResponseRedirect("/") # Redirection en cas d'autentification
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            user = authenticate(username = username, password = password)
+
+            if user:
+                login(request, user) # Fait la variable de session avec l'utilisateur dedans
+
+            else:
+                messages.error(request, "Username or password invalid")
+
         else:
-            messages.error(request, "Username or password invalid")
+            return HttpResponseRedirect("/register") # Redirection en cas d'autentification
 
+    return render(request, "index.html")
 
-
+def connect(request) :
     return render(request, "connect.html")
 
 
