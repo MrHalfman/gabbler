e69f1b03aa17ade397fefde1c1c1c1112a63914c
Billard Martin <martin.l.billard@gmail.com>
2015-05-15 18:19:25 +0200

    Correct an import

--- social/views.py
+++ social/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from models import Gab
+from social.models import Gab
 
 
 @login_required
