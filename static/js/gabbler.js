b77903b2a0ff4633fa829e1ccadbdbd884834d23
Billard Martin <martin.l.billard@gmail.com>
2015-05-20 09:51:28 +0200

    Update the JS part of the logged page
    We will maybe need this script later in another form...

--- static/js/gabbler.js
+++ static/js/gabbler.js
@@ -36,3 +36,80 @@ function displayPart(part) {
     $(".update-part").hide();
     $(part).show();
 }
+
+
+/* ------- DIV INSTEAD OF THE TEXTAREA PART ----------- */
+
+// Check if they are some text in the field (if not, disable the button)
+function emptyText(max, field, button) {
+    if(max - $(field).text().length == max) {
+        $(button).prop('disabled', true);
+    }
+    else {
+        $(button).prop('disabled', false);
+    }
+}
+
+// Copy the text in the div and set it into an hidden field
+function copyToHiddenField(field, hiddenField) {
+    var message = $(field).text();
+    $(hiddenField).val(message);
+}
+
+function focusDiv() {
+    $(this).addClass("on-hover-gab");
+}
+
+// Add CSS class when we click in the div
+function clearHint(field) {
+    $(field).click(function() {
+        focusDiv();
+        $(this).text("");
+    });
+}
+
+function manageBlock(form, field, content, button, max) {
+
+    $(".count").text(max); // Add a max value
+
+    // Verify some conditions before send the gab
+    $(form).submit(function() {
+        copyToHiddenField(field, content);
+    });
+
+    clearHint(field);
+
+    $(field).keyup(function () {
+
+        // decrements the count
+        var preMessage = $(this).text();
+        var charCount = max - preMessage.length;
+        $(".count").text(charCount);
+
+
+        var re = /\S/; // Match if they are only non-visible char
+        if(!re.test(preMessage) || (charCount < 0)) {
+            if(charCount < 0) {
+                $(".count").addClass('gab-overflow');
+            }
+            else {
+                $(".count").removeClass('gab-overflow');
+            }
+            $(button).prop('disabled', true);
+        } else {
+            $(button).prop('disabled', false);
+            emptyText(max, field, button);
+        }
+    });
+
+    // Send the message when we press enter
+    $(field).keypress(function (e) {
+        if(!e.shiftKey) {
+            if(e.which == 13) {
+                copyToHiddenField(field, content);
+                $(form).submit();
+                return false;
+            }
+        }
+    });
+}
